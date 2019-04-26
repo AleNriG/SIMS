6649202b0b56571e75ac1347dfede4f166b00791
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-04-26 13:40:08 +0300

    Add tests for minor calculations

--- tests/test_minor.py
+++ tests/test_minor.py
@@ -2,4 +2,8 @@ from src.modules import minor
 
 
 def test_minor_rsf():
-    assert minor.rsf(7.95e14, 3.32) == 2.39e21
+    assert minor.rsf(7.95e14, 3.32) == 2.394578313253012e+21
+
+def test_minor_hmr(self):
+    assert minor.hmr(30.973762, 30.9816203) == 3941.5346830745675
+
