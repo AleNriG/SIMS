d7bbd7e8ac4883775c640c100dbee4005b2c3318
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-04-28 22:42:32 +0300

    Add test coverage for file reader
    Because of complex type of analyzed data, much simpolier form
    of the inputed data were created.

--- tests/test_file_read.py
+++ tests/test_file_read.py
@@ -6,25 +6,40 @@ from src.modules import file_read
 def reader(filename):
     data = []
     with open(filename, "r") as file:
-        for line in file:
+        for line in file.read().splitlines():
             data.append(line)
     return data
 
 
-with open("tests/testfiles/full_file.dp_rpc_asc", "r") as file:
-    raw_data = []
-    for line in file.read().splitlines():
-        raw_data.append(line)
-start = raw_data.index("*** DATA START ***") + 3
-bad_header = raw_data[start]
+data = reader("tests/files/simple.dp_rpc_asc")
+bad_header = "28Si\t\t\t\t\t31P\t\t\t\t\t75As"
+bad_points = [
+    "4.53333E-002\t\t9.40730E+006\t8.93333E-002\t\t1.27277E+005\t1.29333E-001\t\t6.96442E+003",
+    "1.78667E-001\t\t7.83114E+007\t2.06667E-001\t\t2.21947E+005\t2.46667E-001\t\t4.13942E+003",
+    "2.96000E-001\t\t9.50838E+007\t3.24000E-001\t\t2.93436E+005\t3.64000E-001\t\t2.71346E+003",
+]
 
 
 def test_find_file_name():
-    assert file_read._find_file_name(raw_data) == "ZLN170_2-6_neg"
+    expected = "ZLN170_2-6_neg"
+    assert file_read._find_file_name(data) == expected
 
 
-@pytest.mark.parametrize("given", reader("tests/testfiles/headers"))
-def test_reshape_ion_string(given):
-    print(given)
+def test_reshape_ion_string():
     expected = ["Time", "28Si", "31P", "75As"]
-    assert file_read._reshape_ion_string(given) == expected
+    assert file_read._reshape_ion_string(bad_header) == expected
+
+
+def test_cut_header_and_points():
+    print(file_read._cut_header_and_points(data))
+    expected = (bad_header, bad_points)
+    assert file_read._cut_header_and_points(data) == expected
+
+
+def test_reshape_points():
+    expected = [
+        [4.53333e-002, 9.40730e006, 1.27277e005, 6.96442e003],
+        [1.78667e-001, 7.83114e007, 2.21947e005, 4.13942e003],
+        [2.96000e-001, 9.50838e007, 2.93436e005, 2.71346e003],
+    ]
+    assert file_read._reshape_points(bad_points) == expected
