0a35e07159ef8c0abc7a9fff7b8f518c6ca7e3e7
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-05-06 19:26:12 +0300

    Add plotting for data instance

--- src/modules/data.py
+++ src/modules/data.py
@@ -2,6 +2,8 @@
 TODO: Documentation
 """
 
+from matplotlib import pyplot as plt
+
 
 class Data:
 
@@ -17,6 +19,12 @@ class Data:
         """
         self.name = name
         self.points = points
+
+    def _plot_init(self):
+        """TODO: Docstring for _plot_init.
+        :returns: TODO
+
+        """
         self.figure = self.points.plot(x="Time", title=self.name, grid=True, logy=True)
         self.figure.set(xlabel="Time, [min]", ylabel="Intencity, [imp/sec]")
 
@@ -25,3 +33,8 @@ class Data:
         info = "{0}\nFilename: {1}\n{0}\n{2}".format(border, self.name, self.points)
 
         return info
+
+    def plot(self):
+        """TODO: Docstring for plot."""
+        self._plot_init()
+        plt.show()
