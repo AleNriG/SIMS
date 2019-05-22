9873559d6a43de983c843fa37db6db86a6561e9b
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-05-22 14:15:20 +0300

    Add test for all elements reading from db

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,32 @@
 import pytest
+import csv
+import os
 
 from src.modules import db
+
+
+test_database = os.path.join(os.path.dirname(__file__), "files/test_ions_db.csv")
+
+
+def read_test_db(filepath):
+    ions = []
+    mass = []
+    with open(filepath, 'r') as file:
+        for line in csv.reader(file):
+            x, y = line
+            ions.append(x)
+            mass.append(y)
+    return ions, mass
+
+
+ions, mass = read_test_db(test_database)
 
 
 class TestGetIA:
 
     @pytest.mark.parametrize(
         "key, expected",
-        [('1H', 99.985), ('24Mg', 78.99), ('27Al', 100.0)]
+        [(i, j) for i, j in zip(ions, mass)]
     )
     def test_get_ia(self, key, expected):
         db.get_ia(key) == expected
