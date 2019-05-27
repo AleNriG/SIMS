81c926fad0df826cbec4637c83e57cbb6a03f99e
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-05-27 15:34:03 +0300

    Start select loop function

--- src/selection_menu.py
+++ src/selection_menu.py
@@ -1,3 +1,6 @@
+from typing import List
+from typing import Optional
+
 import cmd2
 
 
@@ -5,15 +8,28 @@ class SelectionMenu(cmd2.Cmd):
 
     """Parental class menus using cmd2 select."""
 
-    def __init__(self, title: str):
+    def __init__(self, title: str) -> None:
         """TODO: to be defined1.
 
         Parameters
         ----------
         title : menu title
 
-
         """
         cmd2.Cmd.__init__(self)
 
         self._title = title.center(30, "~")
+
+    def _selection_menu_loop(self, commands: List[str]) -> Optional[int]:
+        """TODO: Docstring for _selection_menu_loop.
+
+        Parameters
+        ----------
+        commands : TODO
+
+        Returns
+        -------
+        TODO
+
+        """
+        pass
