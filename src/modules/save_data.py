a9835d4491626c7f48141cf6de9427507a4d724b
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-05-30 13:25:20 +0300

    Add basic save to csv function

--- src/modules/save_data.py
+++ src/modules/save_data.py
@@ -1,13 +1,23 @@
-def save(filepath: str):
+import os
+
+from . import data
+
+
+def save(path: str, datafile: data.Data):
     """TODO: Docstring for save.
 
     Parameters
     ----------
-    filepath : TODO
+    path : TODO
 
     Returns
     -------
     TODO
 
     """
-    pass
+    print(os.path.abspath(path))
+    if os.path.isdir(path):
+        datafile.points.to_csv(
+            path_or_buf=os.path.abspath(path) + "/" + datafile.name + ".csv",
+            index=False,
+        )
