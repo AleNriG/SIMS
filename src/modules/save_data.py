72af3141c838456cf50e631e8a1b708ab50de79d
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-05-30 15:01:06 +0300

    Remove testing print line

--- src/modules/save_data.py
+++ src/modules/save_data.py
@@ -15,7 +15,6 @@ def save(path: str, datafile: data.Data):
     TODO
 
     """
-    print(os.path.abspath(path))
     if os.path.isdir(path):
         datafile.points.to_csv(
             path_or_buf=os.path.abspath(path) + "/" + datafile.name + ".csv",
