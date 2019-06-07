76a492cabe7d1d566e3539f68394dd3305a4bd68
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-07 16:30:42 +0300

    Add mock structure determination function

--- src/cli/depth.py
+++ src/cli/depth.py
@@ -1,6 +1,7 @@
 import cmd2
 
 from ..lib import data
+from ..lib.io import manual_input
 
 
 class DepthCalculation(cmd2.Cmd):
@@ -8,6 +9,44 @@ class DepthCalculation(cmd2.Cmd):
     """Depth calculation window"""
 
     def __init__(self, datafile: data.Data):
-        cmd2.Cmd.__init__(self)
+        super().__init__(self)
 
         self._datafile = datafile
+
+    def _set_layers_value(self) -> int:
+        """Input the number of layers in structure
+
+        Returns
+        -------
+        Int number of layers in structure
+
+        """
+        layers = manual_input.read_int(message="Input layers number: ")
+        while layers <= 0:
+            self.poutput("Layers value must be positive!")
+            layers = manual_input.read_int(message="Input layers number: ")
+        return layers
+
+    def _determine_structure(self, layers: int):
+        if layers == 1:
+            self._set_args_to_homostructure()
+        else:
+            self._set_args_to_heterostructure()
+
+    def _set_args_to_homostructure(self):
+        """TODO: Docstring for _set_args_to_homostructure.
+        Returns
+        -------
+        TODO
+
+        """
+        pass
+
+    def _set_args_to_heterostructure(self):
+        """TODO: Docstring for _set_args_to_heterostructure.
+        Returns
+        -------
+        TODO
+
+        """
+        pass
