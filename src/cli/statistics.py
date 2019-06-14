791903557f0fa0a256948c24ef86d2bb0e2d9978
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-14 16:23:41 +0300

    Add printing calculated output

--- src/cli/statistics.py
+++ src/cli/statistics.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from typing import List
 from typing import Tuple
 
@@ -10,7 +11,7 @@ class Statistics(cmd2.Cmd):
 
     """Window for calling statistics calculation"""
 
-    def __init__(self, datafile: data.Data):
+    def __init__(self, datafile: data.Data) -> None:
         """TODO: to be defined1.
 
         Parameters
@@ -26,8 +27,7 @@ class Statistics(cmd2.Cmd):
             self._datafile.ions, "Select column for statistics calculation: "
         )
         mean, std = self._calculate(self._datafile.points[column])
-        self.poutput(f"mean = {mean}")
-        self.poutput(f"std = {std}")
+        self._str({"mean": mean, "std": std})
 
     def _calculate(self, values: List[float]) -> Tuple[float, float]:
         """TODO: Docstring for _calculate.
@@ -43,3 +43,19 @@ class Statistics(cmd2.Cmd):
         """
         result = statistics.mean(values), statistics.std(values)
         return result
+
+    def _str(self, *args: Dict[str, float]):
+        """Print all calculated args
+
+        Parameters
+        ----------
+        args* : TODO
+
+        Returns
+        -------
+        TODO
+
+        """
+        for arg in args:
+            for key, value in arg.items():
+                print(f"{key} = {round(value, 2):.2e}")
